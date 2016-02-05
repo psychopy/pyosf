7427d27f8b510388d7db3d58ee295fed27715a7c
Jon Peirce <jon.peirce@gmail.com>
2016-02-05 16:39:20 +0000

    Created setup scripts for pip install (incl wheels)

--- pyosf/__init__.py
+++ pyosf/__init__.py
@@ -1,9 +1,9 @@
 __version__ = '0.0.1'
-__license__='MIT'
-__author__='Jonathan Peirce'
-__author_email__='jon@peirce.org.uk'
-__maintainer_email__='jon@peirce.org.uk'
-__url__='https://github.com/psychopy/pyosf'
-__downloadUrl__='https://github.com/psychopy/pyosf/releases/'
+__license__ = 'MIT'
+__author__ = 'Jonathan Peirce'
+__author_email__ = 'jon@peirce.org.uk'
+__maintainer_email__ = 'jon@peirce.org.uk'
+__url__ = 'https://github.com/psychopy/pyosf'
+__downloadUrl__ = 'https://github.com/psychopy/pyosf/releases/'
 
-from highlevel import Session, Project
+from highlevel import Project
---
+++ setup.py
@@ -0,0 +1,48 @@
+
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
+from pyosf import __version__
+
+setup(
+    name='pyosf',
+    version=__version__,
+    description='Python lib for synching with OpenScienceFramework projects',
+    long_description=long_description,
+    url='https://github.com/psychopy/pyosf',
+    author='Jon Peirce',
+    author_email='jon.peirce@gmail.com',
+    license='MIT',
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='Open Science Framework PsychoPy',
+    packages=find_packages(exclude=['docs', 'tests']),
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'dev': [],
+        'test': ['pytest', 'coverage'],
+    },
+    package_data={
+        'sample': ['package_data.dat'],
+    },
+)
