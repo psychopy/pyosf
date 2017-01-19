f4993f9ea22559c7fd21e9e6c0a1dc6f68d44799
Erik Kastman <erik.kastman@gmail.com>
2017-01-18 21:42:36 -0500

    Add ‘requests’ dependency to setup.py

--- setup.py
+++ setup.py
@@ -40,8 +40,8 @@ setup(
     keywords='Open Science Framework PsychoPy',
     packages=find_packages(exclude=['docs', 'tests']),
     # $ pip install -e .[dev,test]
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest', 'coverage'],
+    setup_requires=['pytest-runner', 'requests'],
+    tests_require=['pytest', 'coverage', 'requests'],
     package_data={
         'sample': ['package_data.dat'],
     },
